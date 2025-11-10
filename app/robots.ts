4950989fae678e4ebd6952bffaf8eafb1bd6f336
Ashish <ashishgamer473@gmail.com>
2025-11-10 21:18:39 +0530

    your commit message

--- app/robots.ts
+++ app/robots.ts
@@ -19,3 +19,16 @@ import type { MetadataRoute } from "next"
 //     sitemap: `${process.env.NEXT_PUBLIC_APP_URL}/sitemap.xml`,
 //   }
 // }
+// import type { MetadataRoute } from "next"
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/", // allow everything
+        disallow: [], // disallow nothing
+      },
+    ],
+  }
+}
